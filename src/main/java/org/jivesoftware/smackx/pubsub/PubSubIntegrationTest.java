882cae25d9353f6b7c08732c903281c2e7e294cd
Yannick Choni <36097787+yannickchoni@users.noreply.github.com>
2019-11-05 15:15:00 +0200

    Adds a pubsub test that verifies behavior when a node is deleted that does not exist.

--- src/main/java/org/jivesoftware/smackx/pubsub/PubSubIntegrationTest.java
+++ src/main/java/org/jivesoftware/smackx/pubsub/PubSubIntegrationTest.java
@@ -677,7 +677,7 @@ public class PubSubIntegrationTest extends AbstractSmackIntegrationTest {
      * Asserts that the server returns an 'item-not-found' error response when
      * unsubscribing from a node that does not exist.
      *
-     * <p>From XEP-0060 § 6.2.3.3:</p>
+     * <p>From XEP-0060 § 6.2.3.4:</p>
      * <blockquote>
      * If the node does not exist, the pubsub service MUST return an
      * &lt;item-not-found/&gt; error.
@@ -913,6 +913,38 @@ public class PubSubIntegrationTest extends AbstractSmackIntegrationTest {
         }
         finally {
             pubSubManagerOne.deleteNode(nodename);
+        }
+    }
+
+    /**
+     * Asserts that the server returns an 'item-not-found' error response when
+     * deleting a node that does not exist.
+     *
+     * <p>
+     * From XEP-0060 § 8.4.3.2:
+     * </p>
+     * <blockquote> If the requesting entity attempts to delete a node that does not
+     * exist, the service MUST return an &lt;item-not-found/&gt; error.
+     * </blockquote>
+     * 
+     * @throws NoResponseException   if there was no response from the remote
+     *                               entity.
+     * @throws XMPPErrorException    if there was an XMPP error returned.
+     * @throws NotConnectedException if the XMPP connection is not connected.
+     * @throws InterruptedException  if the calling thread was interrupted.
+     */
+
+    @SmackIntegrationTest
+    public void deleteNonExistentNodeTest() throws NoResponseException, XMPPErrorException, NotConnectedException, InterruptedException {
+        final String nodename = "sinttest-delete-node-that-does-not-exist-" + testRunId;
+        try {
+            // Delete an non existent node
+            pubSubManagerOne.deleteNode(nodename);
+            fail("The server should have returned a <item-not-found/> error, but did not.");
+            
+        }
+        catch (XMPPErrorException e){
+            assertEquals(StanzaError.Condition.item_not_found, e.getStanzaError().getCondition());
         }
     }
 }
